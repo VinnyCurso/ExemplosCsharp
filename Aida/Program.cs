ba3ef5e0e721f08cd34d06a397ab04fd91b963f8
vinicius caetano <vinicius caetano@VINCIUS-CAETAN>
2017-03-08 14:07:51 -0300

    Add project files.

---
+++ Aida/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Seech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Aida
+{
+    class Program
+    {
+
+        private static SpeechRecognitionEngine engine = null; //Objetos
+        private static SpeechSynthesizer sp = null; //Objeto
+
+        static void Main(string[] args)
+
+        {
+
+            //Quem ler esse comentario e quiser que eu comente sobre AIDA.
+            //Comenta abaixo o video....
+
+            engine = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-BR");
+            engine.SetInputToDefaulAudioDevice();
+            sp = new SpeechSynthesizer();
+
+
+            String[] conversas = { "ola", "boa noite", "boa tarde ", "tudo bem" };
+
+            Choices c_conversas = new Choices(conversas);
+
+            GrammarBuilder gb_conversas = new GrammarBuilder();
+
+
+
+
+
+        }
+    }
+}
